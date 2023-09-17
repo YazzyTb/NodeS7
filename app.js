375e4dbd957f8577e7f5a9e4077f2ad243f2270e
Jazmin Torrez <jazz.tb.99@gmail.com>
2023-09-17 18:48:35 -0400

    rutas realizadas sin la terminacion .html

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app= express()
 
 app.use(express.static('public'))
 
+app.get('/index',function(req,res){
+    res.sendFile(__dirname+'/public/index.html')
+})
+app.get('/generic',function(req,res){
+    res.sendFile(__dirname+'/public/generic.html')
+})
 
+app.get('/elements',function(req,res){
+    res.sendFile(__dirname+'/public/elements.html')
+})
 
 app.listen(3000)
